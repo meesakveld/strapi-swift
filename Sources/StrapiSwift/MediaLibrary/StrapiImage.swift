7e96e849b37ea9ff1cfe0c86b3900f8e0a1e30a5
meesakveld <meesakveld@gmail.com>
2025-03-04 11:19:25 +0100

    Added getSmallestFormat func to StrapiImage

--- Sources/StrapiSwift/MediaLibrary/StrapiImage.swift
+++ Sources/StrapiSwift/MediaLibrary/StrapiImage.swift
@@ -13,8 +13,8 @@ public struct StrapiImage: Identifiable, Codable {
     public var name: String
     public var alternativeText: String?
     public var caption: String?
-    public var width: Int
-    public var height: Int
+    public var width: Int?
+    public var height: Int?
     public var url: String
     public var formats: ImageFormats?
     public var createdAt: String
@@ -29,6 +29,21 @@ public struct StrapiImage: Identifiable, Codable {
         
         public struct ImageFormat: Codable {
             public var url: String
+        }
+    }
+    
+    // MARK: - Utils
+    public func getSmallestFormat() -> String? {
+        if let thumbnailUrl = formats?.thumbnail?.url {
+            return thumbnailUrl
+        } else if let smallUrl = formats?.small?.url {
+            return smallUrl
+        } else if let mediumUrl = formats?.medium?.url {
+            return mediumUrl
+        } else if let largeUrl = formats?.large?.url {
+            return largeUrl
+        } else {
+            return url
         }
     }
 }
