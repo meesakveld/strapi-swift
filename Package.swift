7a35497356d0ca772892e93b38b75dc8850199e8
meesakveld <meesakveld@gmail.com>
2025-04-24 18:09:19 +0200

    Set minimum iOS version to 15 and Swift tools version to 5.5 #1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "StrapiSwift",
     platforms: [
-        .iOS(.v18),
+        .iOS(.v15),
         .macOS(.v12)
     ],
     products: [
